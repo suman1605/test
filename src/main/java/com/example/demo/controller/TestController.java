a2856e68f0721831c5c3af363225673e3650dc40
Suman Thapa <suman.codehimalaya@gmail.com>
2022-06-04 09:21:35 +0545

    conflict

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,7 +2,11 @@ package com.example.demo.controller;
 
 public class TestController {
 
-    public void test() {
-        System.out.println("hello");
+    private String testDemo() {
+        return "Hi";
+    }
+
+    public String test() {
+        return "hello";
     }
 }
